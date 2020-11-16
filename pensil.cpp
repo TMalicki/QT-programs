a9be88d1348340098298c232ebd91a5e4b314870
Tomasz Malicki <t.malicki1@wp.pl>
2020-11-16 19:47:12 +0100

    Delete pensil.cpp

--- pensil.cpp
+++
@@ -1,9 +0,0 @@
-#include "pensil.h"
-
-Pensil::Pensil(int radius) : mPen{}
-{
-    mPen.setWidth(radius);
-    mPen.setColor(Qt::red);
-    mPen.setStyle(Qt::SolidLine);
-    mPen.setCapStyle(Qt::RoundCap);
-}
